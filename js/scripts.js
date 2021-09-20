1342491fa7955a02e3858823f63620937435dfc3
Marcel-Patrick <schnuffel.hase25@outlook.com>
2021-09-20 21:15:18 +0200

    create initial version
    created initial JS version

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+// Task 1 pop-up Example
+alert('Hello world');
+
+let favoriteFood = 'MY favorite Food is: Yoghurt mit Erdbeeren';
+document.write(favoriteFood); /* display what´s saved in the favoriteFood variable
+                                 in the index page */
+
+
+
+/* Example for written text and errors
+let myName = 'Marcel-Patrick';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+
+
+
+// Example 1
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+// Example 2
+// Now, let’s multiply:
+
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
+
+// Example 3
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize); */
