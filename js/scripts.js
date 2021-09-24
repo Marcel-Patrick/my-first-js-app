2043b866fb07ded27a4f608ada07e68dd69a4848
Marcel-Patrick <schnuffel.hase25@outlook.com>
2021-09-24 16:27:44 +0200

    delete content in scripts.js
    Deleted content js/scripts.js
    
    Related: Task 1.2

--- js/scripts.js
+++
@@ -1,6 +0,0 @@
-// Task 1 pop-up Example
-alert('Hello world');
-
-let favoriteFood = 'MY favorite Food is: Yoghurt mit Erdbeeren';
-document.write(favoriteFood); /* display what´s saved in the favoriteFood variable
-                                 in the index page */
