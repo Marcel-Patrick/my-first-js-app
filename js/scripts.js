8f1c7ff73ef55ac136d86a68c4afc56ad31ddc4e
Marcel-Patrick <schnuffel.hase25@outlook.com>
2021-09-24 20:53:35 +0200

    update scripts.js and add Pokemon List
    Updated js/scripts.js
    Added 7 objects (Pokemon with basic properties) and assign it to array variable
    
    Related: Task 1.2

---
+++ js/scripts.js
@@ -0,0 +1,65 @@
+// The first code line implies: a new variable assigned to a blank array
+let pokemonList = [];
+
+/* The following code lines implies:
+7 of my favourite Pokemon with basic properties; so called: objects */
+let pikachu = {
+  name: 'Pikachu',
+  height: 0.4,
+  weight: 6.0,
+  types: ['electric']
+};
+
+let charizard = {
+  name: 'Charizard',
+  height: 1.7,
+  weight: 90.5,
+  types: ['fire','flying']
+};
+
+let ninetales = {
+  name: 'Ninetales',
+  height: 1.1,
+  weight: 19.9,
+  types: ['fire']
+};
+
+let jigglypuff = {
+  name: 'Jigglypuff',
+  height: 0.5,
+  weight: 5.5,
+  types: ['fairy','normal']
+};
+
+let arcanine = {
+  name: 'Arcanine',
+  height: 1.9,
+  weight: 155.0,
+  types: ['fire']
+};
+
+let mew = {
+  name: 'Mew',
+  height: 0.4,
+  weight: 4.0,
+  types: ['psychic']
+};
+
+let mewtwo = {
+  name: 'Mewtwo',
+  height: 2.0,
+  weight: 122.0,
+  types: ['psychic']
+};
+
+/* The next code line implies: the assign of
+the Pokemon (objects) to the pokemonList array */
+pokemonList = [
+  pikachu,
+  charizard,
+  ninetales,
+  jigglypuff,
+  arcanine,
+  mew,
+  mewtwo
+];
