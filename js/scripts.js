571f81b6b40c5b8d53d68bf2d3060c7aba425f1f
Marcel-Patrick <schnuffel.hase25@outlook.com>
2021-10-29 22:53:08 +0200

    Update smallPokedex at all
    Updated and Added:
    css/pageStyle.css and css/styles.css,
    updated and added button styling classes, scroll functionality of pokemonList array and adjusted header nav-bar styling and hover effects.
    
    Updated and Added:
    index.html,
    updated and added div-tag to body and some css classes to tags and elements.
    
    Updated and Added:
    js/scripts.js,
    updated functions and changed function positions.
    Added two function to show up the message: "Loading Pokemon List...", before & while fetch content from pokemon API and hide the message after loading and display content from pokemon API on the page.
    Updated and added pokemon-Button-Style function, to add a specific color to each pokemon button, after checking the type of the pokemon. For 18 different pokemon types add 18 different color to the pokemon button.
    
    Related: Intro to JavaScript; Task 1.7 Bonus Task

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,9 @@ let pokemonRepository = (function () {
   /* This code line implies: an empty array with later shall show up the objects
   (Pokemon) from an external API in my personal pokemaonList */
   let pokemonList = [];
+
   /* This code line implies: the url to the external API */
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=1000';
 
   /* This code lines implies: a function should add the Pokemon referred to
   with item to the pokemonList array */
@@ -26,6 +27,60 @@ let pokemonRepository = (function () {
     button.addEventListener('click', showDetails.bind(this, pokemon, button));
   }
 
+  /* This code lines implies: a function that show up the
+  message: "Loading Pokemon List..."
+  before & while fetch content from pokemon API */
+  function showLoadingMessage(){
+    let loadingMsg = document.querySelector('main');
+    let button = document.createElement('button');
+    button.innerText = 'Loading Pokemon List...';
+    button.classList.add('pokemon-Button-Style');
+    loadingMsg.appendChild(button);
+  }
+
+  /* This code lines implies: a function that hide the
+  message: "Loading Pokemon List..."
+  after fetched and display all content from pokemon API */
+  function hideLoadingMessage(){
+    let loadingMsg = document.querySelector('main > button');
+    loadingMsg.parentElement.removeChild(loadingMsg);
+  }
+
+  /* This code lines implies: a function to load the details of each pokemon
+  wich is defined in the API URL and puch it inside the pokemonList array */
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    showLoadingMessage();
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      hideLoadingMessage();
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      hideLoadingMessage();
+      console.error(e);
+    });
+  }
+
+  /* This code lines implies: a function to add a specific color to each pokemon
+  button, after checking the type of the pokemon.
+  For 18 different pokemon types add 18 different color to the pokemon button */
+  function addColor(pokemon, button) {
+    let url = pokemon.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      let type = details.types[0].type.name;
+      //call the class based on the pokemon type
+      button.classList.add(type+'-pokemon');
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
   /* This code lines implies: a function to display each pokemon wich is defined
   in the pokemonList inside a button*/
   function addListItem(pokemon) {
@@ -34,7 +89,7 @@ let pokemonRepository = (function () {
     let button = document.createElement('button');
     button.innerText = pokemon.name;
     button.classList.add('pokemon-Button-Style');
-    // pokemonType (pokemon.types[0], button);
+    addColor(pokemon, button);
     listItem.appendChild(button);
     pokemonListElement.appendChild(listItem);
     pokemonEventListener(button, pokemon);
@@ -43,6 +98,7 @@ let pokemonRepository = (function () {
   /* This code lines implies: a function to load each pokemon wich is defined
   in the API URL and puch it inside the pokemonList array */
   function loadList() {
+    showLoadingMessage();
     return fetch(apiUrl).then(function (response) {
       return response.json();
     }).then(function (json) {
@@ -53,32 +109,19 @@ let pokemonRepository = (function () {
         };
         add(pokemon);
       });
+      hideLoadingMessage();
     }).catch(function (e) {
+      hideLoadingMessage();
       console.error(e);
     })
-  }
-
-  /* This code lines implies: a function to load the details of each pokemon
-  wich is defined in the API URL and puch it inside the pokemonList array */
-  function loadDetails(item) {
-    let url = item.detailsUrl;
-    return fetch(url).then(function (response) {
-      return response.json();
-    }).then(function (details) {
-      // Now we add the details to the item
-      item.imageUrl = details.sprites.front_default;
-      item.height = details.height;
-      item.types = details.types;
-    }).catch(function (e) {
-      console.error(e);
-    });
   }
 
   /* This code lines implies: a function to show or display the details of each
   pokemon wich now inside the pokemonList array */
   function showDetails(item) {
-    loadDetails(item);
-    console.log(item);
+    loadDetails(item).then(function () {
+      console.log(item);
+    });
   }
 
   /* This code lines implies: return all the above funkions and values inside
@@ -95,6 +138,8 @@ let pokemonRepository = (function () {
   // This code line implies: the end of the wrap of pokemonList array in an IIFE
 }) ();
 
+/* This code lines implies: the call and of the funkions inside the IIFE to
+display its contents to the outside */
 pokemonRepository.loadList().then(function () {
   pokemonRepository.getAll().forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
