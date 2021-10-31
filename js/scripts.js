7eb2d4dd7dd33228a31dadde624420ef80874a18
Marcel-Patrick <schnuffel.hase25@outlook.com>
2021-10-31 02:38:06 +0200

    Update smallPokedex at all
    Updated and Added:
    css/pageStyle.css and css/styles.css,
    Added css classes and styles for the layout of the new added JS modal.
    Updated and added pokemon-Button-Style classes, to add the specific color to the background of each pokemon modal, after checking the type of the pokemon. For 18 different pokemon types add 18 different color to the pokemon button.
    Added media classes for display behaviour of the app, when screen size is getting smaller.
    
    Updated and Added:
    index.html,
    updated and added div-tag and further buttons related to the creation of JS modal and some css classes to tags and elements.
    
    Updated and Added:
    js/scripts.js,
    updated functions and changed function positions.
    Added new function to show up details of the Pokemon from pokemon API and display them in a modal after clicking the mane button on the page .
    Updated and added pokemon-Button-Style function, to add the specific color to the background of each pokemon modal, after checking the type of the pokemon. For 18 different pokemon types add 18 different color to the pokemon button.
    
    Related: Intro to JavaScript; Task 1.8

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,77 @@
 IIFE will return, and assign the IIFE to that variable.
 Start of wrap of pokemonList array in an IIFE*/
 let pokemonRepository = (function () {
+  /*Start Modal implementatiom */
+  //Create a variable that contains the place of the container
+  let modalContainer = document.querySelector('#modal-container');
 
+  /* This function is used to Create a modal when pokemon is selected from
+  the list */
+  function showModal(pokemon){
+    modalContainer.innerHTML='';
+    let type = pokemon.types[0].type.name;
+    //create new button that contains pokemon details
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+    modal.classList.add(type+'-pokemon');
+    //Create close button
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'X';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    //Create Header tag that contains pokemon name
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = pokemon.forms[0].name;
+    //Create a paragraph tag contains pokemon height
+    let heightElement = document.createElement('p');
+    heightElement.innerText = 'Height: '+pokemon.height/10 +' m';
+    //Create a paragraph tag contains pokemon type
+    let typeElement = document.createElement('p');
+    typeElement.innerText = 'Type: '+type;
+    //Add pokemon imageUrl
+    let img = document.createElement('img');
+    img.classList.add('modal-img');
+    img.src = pokemon.sprites.other['official-artwork']['front_default'];
+
+    //Add the new created tags
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(heightElement);
+    modal.appendChild(typeElement);
+    modal.appendChild(img);
+
+    modalContainer.appendChild(modal);
+    //Add class is-visible to show the modal
+    modalContainer.classList.add('is-visible');
+  }
+  /* This function is used to hide Modal when the use click on cancel or outside
+  of the modal or escape keyboard button */
+  function hideModal() {
+    modalContainer.classList.remove('is-visible');
+  }
+  /* Hide Modal when escape keyboard button is pressed */
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+  /* Hide Modal when we click outside the Modal */
+  modalContainer.addEventListener('click', (e) => {
+    // Since this is also triggered when clicking INSIDE the modal
+    // We only want to close if the user clicks directly on the overlay
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
+  /*End of Modal Creation */
   /* This code line implies: an empty array with later shall show up the objects
   (Pokemon) from an external API in my personal pokemaonList */
   let pokemonList = [];
 
   /* This code line implies: the url to the external API */
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=1000';
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   /* This code lines implies: a function should add the Pokemon referred to
   with item to the pokemonList array */
@@ -47,7 +111,7 @@ let pokemonRepository = (function () {
   }
 
   /* This code lines implies: a function to load the details of each pokemon
-  wich is defined in the API URL and puch it inside the pokemonList array */
+  wich is defined in the API URL+ */
   function loadDetails(item) {
     let url = item.detailsUrl;
     showLoadingMessage();
@@ -56,6 +120,7 @@ let pokemonRepository = (function () {
     }).then(function (details) {
       hideLoadingMessage();
       // Now we add the details to the item
+      showModal(details);
       item.imageUrl = details.sprites.front_default;
       item.height = details.height;
       item.types = details.types;
@@ -123,6 +188,7 @@ let pokemonRepository = (function () {
       console.log(item);
     });
   }
+
 
   /* This code lines implies: return all the above funkions and values inside
   the IIFE to the outside */
